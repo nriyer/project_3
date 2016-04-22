c659eaa7596545f0e1d43d3a53c13179f7e7c43e
NISHA IYER <NRI@NISHAs-Air.home>
2016-04-21 22:29:41 -0400

    deleted files

--- igraph_exploration.R
+++
@@ -1,9 +0,0 @@
-library(igraph)
-library(sna)
-igraph <- load("~/Downloads/astrocollab.Rdata")
-load("football")
-summary(astrocollab)
-#how to simplify
-
-#make igraph an adjacency matrix to do ego.extract
-ego.extract(igraph)
