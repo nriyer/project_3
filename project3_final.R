c6caf2f70b77b6f5f6f042a050b7ddbd1626072a
Samuel Dooley <dooley.samuel@gmail.com>
2016-04-30 18:36:01 -0400

    fixed spelling errors

--- project3_final.R
+++ project3_final.R
@@ -9,257 +9,270 @@ library(NLP)
 library(openNLP)
 library(openNLPdata)
 library(textreuse)
-data("acq")
-head(acq)
 
-#compilation of 50 news articles with additional meta information form the 
+data("acq")
+
+#compilation of 50 news articles with additional meta information form the
 #Reuters-21578 data se of topic acq. 13 documents
 ACQ <- acq
-inspect(ACQ[1:2])
 
-meta(ACQ[[2]], "id") #this is another way to reference 
+#this tell us what information (metadata) about our documents.
+# For example, how many chars are within each doc.
+alldocs <- inspect(ACQ[1:2]) #just the first 2
 
-#extract one document
+# get the first document
 text1 <- ACQ[[1]]
-text1
-names(ACQ)
-#document term matrix
+
+# get the id from the second document
+id.2 <- ACQ[[1]]$meta$id
+id.2 <- meta(ACQ[[1]], "id") #this is another way to reference
+
 ACQdoc <-DocumentTermMatrix(ACQ)
 ACQdoc
-nrow(ACQdoc) #50 rows
-ncol(ACQdoc) #2103 cols
-inspect(ACQdoc[1:6,1:10])
 
-#document term frequency
+inspect(ACQdoc[1:6,1:7])
+
 test1tf <- as.data.frame(termFreq(text1))
 #rank words most to least
-rank_of_words <- cbind(as.data.frame(rownames(test1tf)),test1tf %>% arrange(desc(termFreq(text1))))
+rank_words <- as.data.frame(test1tf[order(test1tf, decreasing = T),])
+head(rank_words)
 
-#convert corpus to lowercase
+# to lower case
 ACQlow <- tm_map(ACQ, content_transformer(tolower))
-ACQlow
 
-#remove anything other than English letters or spaces
+#the next function removes anything other than English letters or spaces
 removeNumPunct <- function(x) gsub("[^[:alpha:][:space:]]*", "", x)
 ACQcl <- tm_map(ACQlow,content_transformer(removeNumPunct))
-ACQcl
 
-#remove stopwords from corpus
+#after converting the text to lower case, and removing punctionation
+#we are going to remove stopwords (filler words such as a, an, the, etc.)
 stopwords <- c(stopwords('english'))
 ACQstop <- tm_map(ACQcl, removeWords, stopwords)
 
-inspect(ACQcl[1:2])
-inspect(ACQ[1:2])
-inspect(ACQstop[1:2])
+#here we can look at the first two text docs and see how the word count (char) differs
+inspect(ACQ[1])
+inspect(ACQstop[1])
 
-#document matrix 2 - inspect once all stopwords, punctuation, numbers taken out
+#now we are putting the terms without punctuation and stopwords into a matrix
 ACQdm2 <- DocumentTermMatrix(ACQstop, control= list(wordLenghts = c(1,Inf)))
 ACQdm2
 
-
-#find terms with a frequency of 5 or more
-freq.terms <- findFreqTerms(ACQdm2, lowfreq=5)
+#find terms with a frequency between 15 and 18
+freq.terms <- findFreqTerms(ACQdm2, lowfreq=15, highfreq = 18)
 freq.terms
 
-#find terms associated with "states"- measure of the co-occurrence of words in multiple documents.
-#words that co-occur with the given word
-#ex below, all words that co occur with states in 50% of the docs
-findAssocs(ACQdm2, "states", 0.25)
-findAssocs(ACQdm2, "year", 0.25)
+#the Assocs function finds associations with terms, such as states or year
+findAssocs(ACQdm2, "states", 0.6)
 
-term.freq <- rowSums(as.matrix(ACQdm2))
-term.freq <- subset(term.freq, term.freq <= 5)
-termdf <- data.frame(term = names(term.freq),freq=term.freq)
-term_sort <- termdf %>% arrange(desc(freq))
-term_sort[1:50,]
-#plot top 50 frequent terms
-ggplot(term_sort[1:50,], aes(x=term,y=freq)) + geom_bar(stat = "identity") + 
-  xlab("Terms") + ylab("Count") + coord_flip()
-
-#########################################################################################
-inspect(ACQcl)
-
-#we're going to start with the document after punctuation has been removed
-dtm <- DocumentTermMatrix(ACQcl)
-#remove sparse terms, leaving a matrix that is 10% empty space, max
-dtms <- removeSparseTerms(dtm, 0.1)
-inspect(dtms)
-removeWords
-top10 <- sort(rowSums(as.matrix(dtms)), decreasing = TRUE)[1:10] #use dtm for with sparsity
-top10 <- as.data.frame(top10)
-top10
-myStopwords <- c(stopwords('english'))
-inspect(ACQcl) #without punctuation 
-doc7 <- ACQcl[1] #doc7 has 73 words 
-doc7 <- tm_map(doc7, removeWords, myStopwords) #remove stopwords 
-doc7m <- DocumentTermMatrix(doc7) 
-doc7m <- removeSparseTerms(doc7m, 0.25)
-inspect(doc7m)
-doc7m
-
-
-################################################
 
 #using quanteda for the next few questions
+data("acq")
 mycorpus <- corpus(acq)
 summary_acq <- as.data.frame(summary(mycorpus))
-#find top 10 longest docs
 
-#dendogram for top 10
-#list of top10 docs
 #10 longest documents in the corpus
 sort_top10 <- summary_acq %>% arrange(desc(Tokens))
 top_10_docs <- subset(sort_top10, select=c(id, heading))[1:10,]
 top10 <- top_10_docs[,1]
+topdocs <- mycorpus[mycorpus$documents$id %in% top10]
 
 top10
-topdocs <- mycorpus[mycorpus$documents$id %in% top10]
-str(topdocs[[1]])
-
-#sumarry of whole doc and top docs as data frame
-summary_td <- as.data.frame(summary(topdocs))
 
 
-#dendogram for top 10
-acq.mat <- as.matrix(t(tdm2))
-acq.mat <- as.data.frame(acq.mat)
-acq.mat <- acq.mat[,top10]
-acq.mat <- as.matrix(acq.mat)
-
-distMatrix <- dist(scale(acq.mat[,1]))
-fit <- hclust(distMatrix, method = "ward.D2")
-plot(fit,main = "Dendogram")
 
 #top 10 dendogram, 1 for each of the top 10 documents
-top10.dendogram <- function(tdm2,doc)
+top10.dendogram <- function(doc)
 {
-  acq.mat <- as.matrix(t(tdm2))
-  acq.mat <- as.data.frame(acq.mat)
-  acq.mat <- acq.mat[,top10]
-  acq.mat <- as.matrix(acq.mat)
-  distMatrix <- dist(scale(acq.mat[,doc]))
+  # full dendogram
+  acq.mat <- as.matrix( ACQdm2[doc,] )
+  distMatrix <- dist(scale(acq.mat[doc,]))
   fit <- hclust(distMatrix, method = "ward.D2")
-  print(plot(fit,main = "Dendogram"))
+  plot(fit,main = paste("Full Dendogram for ID: ", doc), xlab ="", sub = "")
+
+  # dendogram with sparse terms removed
+  acq.sp <- removeSparseTerms(ACQdm2, sparse = .8)
+  acq.mat <- as.matrix( acq.sp[doc,] )
+  distMatrix <- dist(scale(acq.mat[doc,]))
+  fit <- hclust(distMatrix, method = "ward.D2")
+  plot(fit,main = paste("Dendogram without sparse terms for ID: ", doc), xlab = "", sub = "")
 }
 for (i in 1:10){
-top10.dendogram(tdm2,i)
+  top10.dendogram(top10[i])
 }
+
+# full dendogram
+acq.mat <- as.matrix(ACQdm2)
+distMatrix <- dist(scale( colSums(acq.mat[top10,]) ))
+fit <- hclust(distMatrix, method = "ward.D2")
+plot(fit,main = paste("Full Dendogram for Top 10 Documents"), xlab ="", sub = "")
+
+# dendogram with sparse terms removed
+acq.sp <- removeSparseTerms(ACQdm2, sparse = .8)
+acq.mat <- as.matrix(acq.sp)
+distMatrix <- dist(scale( colSums(acq.mat[top10,]) ))
+fit <- hclust(distMatrix, method = "ward.D2")
+plot(fit,main = paste("Dendogram for Top 10 without Sparse Terms"), xlab ="", sub = "")
+
+
+options(warn=-1)
 #word cloud for top 10
 wordcloud.func <- function(ACQstop, doc)
 {
   dtm <- TermDocumentMatrix(ACQstop)
-  m <- as.data.frame(as.matrix(dtm))
-  m <- m[,top10]
-  m <- as.matrix(m)
-  v <- sort(m[,doc],decreasing=TRUE)
-  d <- data.frame(word = row.names(m),freq=v)
+  v <- as.matrix(dtm[,doc])
   set.seed(1234)
-  print(wordcloud(words = d$word, freq = d$freq, min.freq = 1,
-            max.words=200, random.order=FALSE, rot.per=0.35, 
-            colors=brewer.pal(8, "Dark2")))
+
+  layout(matrix(c(1, 2), nrow=2), heights=c(0.5, 4.5))
+  par(mar=rep(0, 4))
+  plot.new()
+  text(x=0.5, y=0.5, paste("Word Cloud for Document ID: ",doc) )
+  wordcloud(words = rownames(v), freq = v, min.freq = 1,
+            max.words=200, random.order=FALSE, rot.per=0.35,
+            colors=brewer.pal(8, "Dark2"))
 }
+
 for (i in 1:10){
-  wordcloud.func(ACQstop,i)
+  wordcloud.func(ACQstop,top10[i])
 }
 
-#####FIND LONGEST WORD in 10 docs
-max_length <- c()
-word <- c()
-id <- c()
-for (i in 1:10){
-  words <- tokenize_words(topdocs[[i]][[1]])
-  word[i] <- words[nchar(words) == max(nchar(words))]
-  max_length[i] <- max(nchar(words))
-  id[i] <- names(topdocs[i])
-}
+dtm <- TermDocumentMatrix(ACQstop)
+v <- t( as.matrix(dtm[,top10]) )
+v <- colSums(v)
+set.seed(1234)
 
-final.longest_word <- data.frame(max_length = max_length,word=word,id = id)
+layout(matrix(c(1, 2), nrow=2), heights=c(0.5, 4.5))
+par(mar=rep(0, 4))
+plot.new()
+text(x=0.5, y=0.5, "Word Cloud for Top Documents" )
+wordcloud(words = names(v), freq = v, min.freq = 1,
+          max.words=200, random.order=FALSE, rot.per=0.35,
+          colors=brewer.pal(8, "Dark2"))
 
-#####FIND LONGEST SENTENCE in 10 docs
-topdocs
-topdocs[[2]]
-names(topdocs[1])
+options(warn=0)
 
-#split into sentences
-get_sentence_df_func <- function(x){
-  sentence_df <- data.frame(sentence = character(0),
-                            document = character(0))
-  for (i in 1:10){
-    temp <- data.frame(sentence=tokenize_sentences(x[[i]][[1]]),id=names(x[i]))
-    sentence_df <- rbind(sentence_df,temp)
+# find the longest word and longest sentence in each document from the 10 largest documents
+for (i in 1:10) {
+  ind <- top10[i]
+  s <- as.String(acq[[ind]]$content)
+
+  ##longest sentence by characters
+  sent_token_annotator <- Maxent_Sent_Token_Annotator()
+  a1 <- sent_token_annotator(s)
+  l <- a1$end - a1$start # table of sentence lengths
+  ls.i <- which.max(l) #index of longest sentence by characters
+  ls <- as.String( s[a1][ls.i] ) #longest sentence by characters
+
+  ##longest sentence by constituents
+  word_token_annotator <- Maxent_Word_Token_Annotator()
+  a2 <- word_token_annotator(s, a1)
+  a2 <- a2[a2$type=="sentence"]
+  l.w <- as.matrix( lapply(a2$features, function(x) length(x$constituents)) ) #sent length
+  l.w.ind <- which.max( l.w )
+  ls.w <- as.String( s[a1][l.w.ind] )
+
+  ##longest word
+  word_token_annotator <- Maxent_Word_Token_Annotator()
+  a2 <- word_token_annotator(s, a1)
+  a2 <- a2[a2$type=="word"]
+  lw.i <- which.max(a2$end - a2$start) #index of longest sentence
+  lw <- s[a2][lw.i] #longest sentence
+
+  # print everything so that it's pretty
+  print( as.String( paste("Document ID: ", ind) ) )
+  print( as.String( paste( "\tLongest Word:\t", lw) ) )
+  if (ls == ls.w) {
+    print( as.String( paste( "\tLongest Sentence:\t", ls) ) )
+  } else {
+    print( as.String( paste( "\tLongest Sentence by nchar:\t", ls) ) )
+    print( as.String( paste( "\tLongest Sentence by words:\t", ls.w) ) )
+    print( as.String(""))
   }
-  return(sentence_df)
-}
-#data frame of sentences by document 
-text_sent <- get_sentence_df_func(topdocs)
+  print( as.String(""))
 
-
-#word count for each sentence
-text_sent$sentence <- as.character(text_sent$sentence)
-count <- c()
-sapply(strsplit(text_sent$sentence[23], " "), length)
-for (i in 1:nrow(text_sent)){
-  count[i] <- sapply(strsplit(text_sent$sentence[i], " "), length)
-}
-#length of each sentence in each document
-count_sentences <- cbind(count,text_sent)
-#top 10 lengths 
-longest_10 <- count_sentences %>% group_by(id) %>% 
-  arrange(desc(count)) %>% top_n(1,count) %>% distinct(id)
-
-
-#remove punctuation for each sentence
-#remove punctuation from topdocs
-str(count_sentences$sentence)
-count_sentences$sentence_nopunct[4] <- ( gsub("[[:punct:]]", "", count_sentences$sentence[4]) )
-#loop to remove punctuation from each sentence
-nopunct <- c()
-for (i in 1:nrow(count_sentences)){
-  nopunct[i] <-  ( gsub("[[:punct:]]", "", count_sentences$sentence[i]) )
-}
-#bind final df together
-final_nopunct_df <- cbind(nopunct,count_sentences)
-
-########################################
-#part of speech                     ####
-########################################
-str(topdocs)
-library(koRpus)
-# perform POS tagging
-corpus_top10 <- Corpus(VectorSource(topdocs))
-writeCorpus(corpus_top10)
-text.tagged <- treetag("1.txt", treetagger="manual", lang="en",
-                       TT.options=list(path="/Applications/tree-tagger-MacOSX-3.2-intel", preset="en"))
-#find out why lib is not in tree tagger...then the above should work.
-
-
-########
-#parts of speech - not complete, we have a list of words. all tht is left is tagging parts of speech
-all_words <- c()
-id.all <- c()
-
-for (i in 1:10){
-  all_words <- tokenize_words(topdocs[[i]][[1]])
-  id.all[i] <- names(topdocs[i])
 }
 
-all.words <- as.data.frame(all_words)
-all.words$all_words <- as.character(all.words$all_words)
 
 
-#################################################################################
-##not sure this works, backup
+d.full <- data.frame()
+for (i in 1:10) {
+  ind <- top10[i]
+  s <- as.String(acq[[ind]]$content)
 
-#for zipfr
-library(languageR)
-document.spc <- text2spc.fnc(strsplit(x[[1]][1]$content," "))
-document.vgc <- growth.fnc(strsplit(acq10[[1]][1]$content," "))
-text <- strsplit(document$content, " ")[[1]]
+  ##longest sentence by characters
+  sent_token_annotator <- Maxent_Sent_Token_Annotator()
+  a1 <- sent_token_annotator(s)
+  l <- a1$end - a1$start # table of sentence lengths
 
-##create spc object
-this.spc <- text2spc.fnc(text)
+  ##longest sentence by constituents
+  word_token_annotator <- Maxent_Word_Token_Annotator()
+  a2 <- word_token_annotator(s, a1)
+  a2 <- a2[a2$type=="sentence"]
+  l.w <- as.matrix( lapply(a2$features, function(x) length(x$constituents)) ) #sent length
 
-#create growth object
-this_growth<- growth.fnc(text)
-all.words[1,]
 
+  d<-data.frame(id=ind,lenbychar=l,lenbyword=l.w,sent=sapply( s[a1], function(x) substr(x,0,45)))
+  d.full <- rbind(d.full,d)
+}
+rownames(d.full) <- 1:dim(d.full)[1]
+print(d.full)
+
+d <- data.frame()
+pos_tag_annotator = Maxent_POS_Tag_Annotator()
+for (i in 1:10) {
+
+  ind <- top10[i]
+  s <- as.String(acq[[ind]]$content)
+  s <- tolower(s)
+
+  a1 <- sent_token_annotator(s)
+
+  # For each sentence of each document, remove the punctuation.
+  s.cl <- sapply( s[a1], removePunctuation )
+
+  for( j in 1:length(s.cl)) {
+
+    sub.s <- as.String(s.cl[j])
+
+    a2 <- word_token_annotator(sub.s, a1)
+    a3 <- pos_tag_annotator( sub.s, a = a2)
+
+    words <- as.vector( sub.s[a3] )
+    pos <- as.matrix( lapply(a3$features, function(x) x$POS) )
+
+    df <- data.frame(words=words, pos=pos)
+    d<-rbind(d,df)
+  }
+}
+nums <-sapply( d$words, function(x) suppressWarnings(!is.na(as.numeric(as.character(x)))))
+d.smaller <- d[!nums,]
+d.smaller <- unique(d.smaller)
+d.smaller <- d.smaller[order(d.smaller$words),]
+head(d.smaller)
+
+
+## Analyze word frequency using functions from package zipfR
+library(zipfR)
+for(i in 1:10) {
+  ind <- top10[i]
+  f <- as.matrix(ACQdm2[ind,]) # frequencies for all words
+
+  z <- tfl(f) #
+  plot(sort(z$f,decreasing=TRUE), xlab="rank",ylab="frequency",
+       main=paste("Type Frequency Plot for ID: ",acq[[ind]]$meta$id))
+}
+
+f <- colSums(as.matrix(ACQdm2[top10,])) # frequencies for all words
+z <- tfl(f) #
+plot(sort(z$f,decreasing=TRUE), xlab="rank",ylab="frequency",
+     main="Type Frequency Plot for Top 10")
+
+
+
+     for(i in 1:10) {
+       ind <- top10[i]
+       f <- as.matrix(ACQdm2[ind,]) # frequencies for all words
+
+       z <- tfl(f) #
+       s <- tfl2spc(z)
+       plot(s, main=paste("Frequency Spectrum for ID: ",acq[[ind]]$meta$id))
+     }
