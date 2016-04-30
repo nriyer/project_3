03dd14736ebc21585e3536ac3cfc50029c24e4ca
Samuel Dooley <dooley.samuel@gmail.com>
2016-04-30 17:20:29 -0400

    only have pos and freq left

--- project 3/tm_SD.R
+++ project 3/tm_SD.R
@@ -1,5 +1,5 @@
 library(tm)
-library(dplyr)
+library(zipfR)
 library(wordcloud)
 library(quanteda)
 library(wordnet)
@@ -35,73 +35,86 @@ for (i in 1:10) {
 }
 
 # find the longest word and longest sentence in each document from the 10 largest documents.
+word <- c()
+sentence <-c()
+id <- c()
 for (i in 1:10) {
-  ind <- top10rowid[i]
+  ind <- top10[i]
+  id[i] <- ind
   s <- as.String(acq[[ind]]$content)
   
   ##longest sentence
   sent_token_annotator <- Maxent_Sent_Token_Annotator()
-  a1 <- annotate(s, sent_token_annotator)
+  a1 <- sent_token_annotator(s)
   l <- a1$end - a1$start # table of sentence lengths
   ls.i <- which.max(l) #index of longest sentence
   ls <- s[a1][ls.i] #longest sentence
   
   acq[[ind]]$meta$ls <- ls #add longest sentence to metadata of document
+  sentence[i] <- ls
   
   # Print a table of the length of each sentence in each of the 10 documents.
   df <- as.data.frame(a1)
   df$length <- l
-  print(df)
+  #print(df)
   
   ##longest word
   word_token_annotator <- Maxent_Word_Token_Annotator()
-  a2 <- annotate(s, word_token_annotator, a1)
+  a2 <- word_token_annotator(s, a1)
   a2 <- a2[a2$type=="word"]
   lw.i <- which.max(a2$end - a2$start) #index of longest sentence
   lw <- s[a2][lw.i] #longest sentence
+  print( c(ind, lw))
   
   acq[[ind]]$meta$lw <- lw #add longest word to metadata of document
+  word[i] <- lw
   
 }
+data.frame(id = id, word=word, sentence=sentence)
 
-
-## DOES NOT USE WORDNET PACKAGE...
 
 
 # For each sentence of each document, remove the punctuation.
 # For each word print its part of speech using the wordnet packages.
+d<-data.frame()
 for (i in 1:10) {
   
   ind <- top10rowid[i]
   s <- as.String(acq[[ind]]$content)
+  s <- tolower(s)
   
-  ##longest sentence
-  sent_token_annotator <- Maxent_Sent_Token_Annotator()
-  a1 <- annotate(s, sent_token_annotator)
+  a1 <- sent_token_annotator(s)
   
   # For each sentence of each document, remove the punctuation.
   s.cl <- sapply( s[a1], removePunctuation )
   
   for( j in 1:length(s.cl)) {
-    
-    print (as.String(s.cl[j])) # show we stripped punctuation
-    
-    sub.s <- as.String(s[a1][j])
-    
-    pos_tag_annotator <- Maxent_POS_Tag_Annotator()
-    a3 <- annotate(sub.s,
-                   list(sent_token_annotator,
-                        word_token_annotator,
-                        pos_tag_annotator))
 
-    df <- as.data.frame(a3)
-    df$words <- sub.s[a3]
-    df$pos <- lapply(df$features, function(x) x$POS)
-    print(df[2:dim(df)[1],6:7])
+    sub.s <- as.String(s.cl[j])
+    
+    a2 <- word_token_annotator(sub.s, a1)
+    a3 <- pos_tag_annotator( sub.s, a = a2)
+
+    words <- as.vector( sub.s[a3] )
+    pos <- as.matrix( lapply(a3$features, function(x) x$POS) )
+    
+    df <- data.frame(words=words, pos=pos)
+    d<-rbind(d,df)
     
 
-    
   }
 
   
+}
+
+#################################################################################
+#for zipfr
+
+for(i in 1:10) {
+  ind <- top10rowid[i]
+  words <- colnames(as.matrix(dtm[ind,])) # all words
+  f <- as.matrix(dtm[ind,]) # frequencies for all words
+
+  z <- tfl(f)
+  plot(sort(z$f,decreasing=TRUE), xlab="rank",ylab="frequency", main=acq[[ind]]$meta$id)
 }
